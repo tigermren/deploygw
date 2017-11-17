8045de835deb5fe28d4d9bdc649a3e8cab1c0150
Xiaoyin Ren <duckrxy@hotmail.com>
2017-11-17 23:50:43 +0800

    rm file silently

--- generate_ipset.sh
+++ generate_ipset.sh
@@ -1,10 +1,10 @@
-rm cidr_cn
+rm -f cidr_cn
 curl -sL http://f.ip.cn/rt/chnroutes.txt | egrep -v '^$|^#' > cidr_cn
 sudo ipset destroy cidr_cn
 sudo ipset -N cidr_cn hash:net
-rm ipset.sh
+rm -f ipset.sh
 for i in `cat cidr_cn`; do echo ipset -A cidr_cn $i >> ipset.sh; done
 chmod +x ipset.sh && sudo ./ipset.sh
-rm ipset.cidr_cn.rules
+rm -f ipset.cidr_cn.rules
 sudo ipset -S > ipset.cidr_cn.rules
 sudo cp ./ipset.cidr_cn.rules /etc/ipset.cidr_cn.rules
